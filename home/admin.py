c124d8dad7f9b6e933428502dc25883f5cb451fc
theWiz01 <omatanmi118@gmail.com>
2023-07-07 22:34:02 +0100

    admin

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from home.models import Student
+from django.contrib.auth.admin import UserAdmin
+from home.models import Student, User
 # Register your models here.
 
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Student)
